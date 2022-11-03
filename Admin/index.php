6658ed8976d70dad947a8b009c7b27917aa806d8
sanjeev <samphpedu17@gmail.com>
2022-11-03 11:38:51 +0545

    project

--- Admin/index.php
+++ Admin/index.php
@@ -1,8 +1,60 @@
 <!DOCTYPE html>
-    <head>
-        <title>php</title>
-    </head>
-    <body>
-        
-    </body>
-</html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <link rel="stylesheet" href="../css/admin.css">
+            <title>Admin Panel</title>
+        </head>
+        <body>
+            <!-- Menu section starts -->
+                <section class="menu-bar">
+                    <div class="wrapper">
+                       <div class="logo">
+                        <img src="../images/logo.png" class="img-responsive" alt="">
+                       </div>
+                       <div class="menu">
+                            <ul>
+                                <li><a href="#">dashboard</a></li>
+                                <li><a href="#">user</a></li>
+                                <li><a href="#">category</a></li>
+                                <li><a href="#">Product</a></li>
+                                <li><a href="#">order</a></li>
+                            </ul>
+                       </div>
+                       <div class="clearfix"></div>
+                    </div>
+                </section>
+             <!-- Menu section ends -->
+
+                  <!-- Body section starts -->
+                  <section class="content">
+                    <div class="wrapper">
+                       <div class="breadcumb ">
+                            <ul>
+                                <li><a href="#">dashboard</a></li>
+                                <li><a href="#">dashboard</a></li>
+                            </ul>
+                       </div>
+                       <div class="from-wrapper">
+                        <form action="">
+
+                        </form>
+                       </div>
+                    </div>
+                </section>
+             <!-- Body section ends -->
+
+                  <!-- Footer section starts -->
+                  <section class="footer">
+                    <div class="wrapper text-center">
+                        <p>Copyright <?php echo date('Y'); ?>..All Rights Reserved. Designed & develpoed by<a class="footer-link" href="#"> S.A.M</a></p>
+                    </div>
+                </section>
+             <!-- Footer section ends -->
+
+             
+        </body>
+       
+ </html>
