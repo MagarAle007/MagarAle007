af6ab70a49f052d8745aa77767eeff781fe4c644
sanjeev <eduwebsam19@gmail.com>
2023-03-12 14:15:36 +0545

    real_state

--- index.php
+++ index.php
@@ -1,171 +1,111 @@
-<?php 
-    include('layout/header.php');
+<?php
+ include('layout/header.php'); 
 ?>
-<body>
-    <!--search Section Begins Here -->
-    <section class="search text-center">
+<!--Search Bar Begins Here-->
+<section class="search text-center">
         <div class="container">
-            <form>
-                <input type="search" name="search" placeholder="Search Here...." id="search">
-              <a href="#">  <input type="button" value="search" class="btn btn-primary"></a>
+            <form method="GET" action="search-results.php">
+                <input type="search" name="query" id="search" placeholder="Search for Food here...">
+                <input type="submit" value="Search" class="btn btn-primary">
             </form>
         </div>
     </section>
-    <!--search Section Begins Here -->
+    <!--Search Ends Here-->
 
-    <!-- catagories Section Begins Here -->
-    <section class="categories text-center">
+    <!--Categories Begins Here-->
+    <section class="categories">
         <div class="container">
-            <h2 class="text-center">categories</h2>
+            <h2 class="text-center">Categories</h2>
+            <?php
+                //create the sql to pull the categories
+                $sql = "SELECT * FROM CATEGORIES WHERE FEATURED='YES' AND status='yes' LIMIT 3";
 
-            <div class="card float-container">
-                <a href="categories.html"><img src="images/cat1.jpg" alt="House" class="img-responsive img-rounded"></a>
-                <h4 class="float-text text-white">Raw land</h4>
-            </div>
-            
-            <div class="card float-container">
-                <a href="categories.html"><img src="images/cat2.jpg" alt="House" class="img-responsive img-rounded"></a>
-            <h4 class="float-text text-white">Commercial</h4>
-            </div>
+                //execute the query using connection
+                $execute = mysqli_query($conn,$sql);
 
-            <div class="card float-container">
-                <a href="categories.html"><img src="images/cat2.jpg" alt="House" class="img-responsive img-rounded"></a>
-                <h4 class="float-text text-white">Special Use</h4>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-    </section>/
-    <!-- catagories Section Begins Here -->
+                //if execution is correctly submitted
+                if($execute == TRUE){
 
-    <!-- Explore Section Begins Here -->
-    <section class="land-menu">
-        <div class="container">
-            <h1>Explore Lands</h1>
+                    //count the number of rows
+                    $count = mysqli_num_rows($execute);
 
-            <div class="box">
-               <div class="box-image">
-                 <img src="images/cat1.jpg" alt="menu raw" class="img-responsive img-rounded">
-               </div>
+                    if($count > 0){
+                       //if count is greater than 0 we will execute the loop
 
-                <div class="box-desc">
-                    <h4>Raw land</h4>
-                    <p class="item-price">$10,001</p>
-                    <p class="item-desc">sdskfs</p>
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="box-image">
-                    <img src="images/cat1.jpg" alt="menu raw" class="img-responsive img-rounded">
-                  </div>
-
-                   <div class="box-desc">
-                       <h4>Raw land</h4>
-                       <p class="item-price">$10,001</p>
-                       <p class="item-desc">sdskfs</p>
-                       <a href="#" class="btn btn-primary">Order Now</a>
-                   </div>
-            </div>
-
-            <div class="box">
-                <div class="box-image">
-                    <img src="images/cat1.jpg" alt="menu raw" class="img-responsive img-rounded">
-                  </div>
-
-                   <div class="box-desc">
-                       <h4>Raw land</h4>
-                       <p class="item-price">$10,001</p>
-                       <p class="item-desc">sdskfs</p>
-                       <a href="#" class="btn btn-primary">Order Now</a>
-                   </div>
-            </div>
-
-            <div class="box">
-                <div class="box-image">
-                    <img src="images/cat1.jpg" alt="menu raw" class="img-responsive img-rounded">
-                  </div>
-
-                   <div class="box-desc">
-                       <h4>Raw land</h3>
-                       <p class="item-price">$10,001</p>
-                       <p class="item-desc">sdskfs</p>
-                       <a href="#" class="btn btn-primary">Order Now</a>
-                   </div>
-            </div>
-
-            <div class="box">
-
-                <div class="box-image">
-                    <img src="images/cat1.jpg" alt="menu raw" class="img-responsive img-rounded">
-                  </div>
-
-                   <div class="box-desc">
-                       <h3>Raw land</h3>
-                       <p class="item-price">$10,001</p>
-                       <p class="item-desc">sdskfs</p>
-                       <a href="#" class="btn btn-primary">Order Now</a>
-                   </div>
-
-            </div>
-
-            <div class="box">
-
-                <div class="box-image">
-                    <img src="images/cat1.jpg" alt="menu raw" class="img-responsive img-rounded">
-                  </div>
-
-                   <div class="box-desc">
-                       <h3>Raw land</h3>
-                       <p class="item-price">$10,001</p>
-                       <p class="item-desc">sdskfs</p>
-                       <a href="#" class="btn btn-primary">Order Now</a>
-                   </div>
-
-            </div>
+                        while($rows=mysqli_fetch_assoc($execute)){
+                            $id = $rows['id'];
+                            $category_title = $rows['title'];
+                            $category_image = $rows['image_name'];
+                            ?>
+                            <a href="categories.php">
+                                <div class="card float-container">
+                                    <img src="images/category/<?php echo $category_image; ?>" alt="<?php echo$category_title; ?>" class="img-responsive img-rounded">
+                                    <h3 class="float-text text-white"><?php echo$category_title; ?></h3>
+                                </div>
+                            </a>
+                            <?php
+                        }
+                    }else{
+                        //
+                        echo "<div class='text-center'>No category Found</div>";
+                    }
+                    
+                }
+            ?>
+          
             <div class="clearfix"></div>
         </div>
     </section>
-    <!-- Explore Section Begins Here -->
+    <!--Categories Ends Here-->
 
-    <!-- social Section Begins Here -->
-    <section class="social text-center">
+    <!--Explore Begins Here-->
+    <section class="food-menu">
         <div class="container">
-            <h1>Connect us in.</h1>
-            <ul>
-                <li>
-                    <a href="#" target="_blank">
-                        <img src="https://img.icons8.com/color/48/000000/facebook.png"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" target="_blank">
-                        <img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" target="_blank">
-                        <img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" target="_blank">
-                        <img src="https://img.icons8.com/fluency/48/000000/youtube.png"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" target="_blank">
-                        <img src="https://img.icons8.com/fluency/48/000000/linkedin.png"/>
-                    </a>
-                </li>
-            </ul>
+            <h2 class="text-center">Explore Products</h2>
+            <?php
+                //sql to fetch products
+
+                $product_sql = "SELECT * FROM products WHERE featured = 'yes' AND status = 'yes' limit 8";
+
+                //execute this sql
+                $pro_exec = mysqli_query($conn,$product_sql);
+
+                //check if execution is true
+                if($pro_exec == TRUE){
+                    //count the numer of rows
+                    $count = mysqli_num_rows($pro_exec);
+                    
+                    if($count > 0){
+                        //run the while loop
+                        while($pro_rows= mysqli_fetch_assoc($pro_exec)){
+                           $pro_id = $pro_rows['id'];
+                           $pro_title = $pro_rows['title'];
+                           $pro_description = $pro_rows['description'];
+                           $pro_price = $pro_rows['price'];
+                           $pro_image = $pro_rows['image_name'];
+                           ?>
+                            <div class="box">
+                                <div class="box-img">
+                                    <img src="images/product/<?php echo $pro_image; ?>" alt="<?php echo $pro_title; ?>" class="img-responsive img-rounded">
+                                </div>
+                                <div class="box-desc">
+                                    <h4><?php echo $pro_title; ?></h4>
+                                    <p class="item-price">Rs.<?php echo $pro_price; ?></p>
+                                    <p class="item-desc"><?php echo $pro_description; ?></p>
+                                    <a href="<?php echo APP_URL; ?>order.php?id=<?php echo $pro_id; ?>" class="btn btn-primary">Order Now</a>
+                                </div>
+                            </div>
+                            <?php
+                        }
+                    }else{
+                        echo "<div class='text-center'>No Products found</div>";
+
+                    }
+                }
+            ?>
+            <div class="clearfix"></div>
         </div>
     </section>
-    <!-- social Section Begins Here -->
+    <!--Explore Ends Here-->
 
-    <?php
-        include('layout/footer.php');
-
-    ?>
-</body>
-</html>
+    <?php include('layout/footer.php'); ?>
--- layout/header.php
+++ layout/header.php
@@ -1,30 +1,33 @@
-
+<?php include('admin/config/constants.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Real_state</title>
+
+    <link rel="stylesheet" href="css/style1.css">
+    <title>Hamro Sekuwa</title>
 </head>
+
 <body>
-    <!-- Navbar Section Begins Here -->
+    <!--NavBarSection Begins Here-->
     <section class="navbar">
         <div class="container">
             <div class="logo">
-                <a href="index.php"> <img src="images/logo.png" alt="Real_state logo" class="img-responsive"></a>
+                <img src="images/HamroSekuwaLogo1.png" alt="HamroSekuwalogo" class="img-responsive">
             </div>
             <div class="menu text-right">
                 <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="categories.php">categories</a></li>
-                        <li><a href="land.php">Lands</a></li>
-                        <li><a href="contact.php">Contact</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="Categories.php">Categories</a></li>
+                    <li><a href="product.php">Product</a></li>
+                     <li><a href="contact.php">Contact</a></li>
+                    
                 </ul>
             </div>
             <div class="clearfix"></div>
         </div>
-    
     </section>
-    <!-- Navbar Section Begins Here -->
+    <!--NavBarSection Ends Here-->
